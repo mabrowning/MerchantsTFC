62fa3bc6292f3c845da0b6075d0361b86163d734
flametaichou <lein.flamesword@gmail.com>
2019-05-02 01:12:58 +0300

    Stall Q toss fix

--- src/com/aleksey/merchants/Containers/Slots/SlotStall.java
+++ src/com/aleksey/merchants/Containers/Slots/SlotStall.java
@@ -1,5 +1,7 @@
 package com.aleksey.merchants.Containers.Slots;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
@@ -10,9 +12,17 @@ public class SlotStall extends Slot
     {
         super(iinventory, slotIndex, x, y);
     }
+
     @Override
     public boolean isItemValid(ItemStack itemstack)
     {       
         return true;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public boolean func_111238_b()
+    {
+        return false;
     }
 }
